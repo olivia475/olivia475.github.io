920e73e14ef1d87d6b30c6ed77d4b8a72bc1115a
Olivia Blackmore <olivia_blackmore@sfu.ca>
2026-05-25 23:39:20 -0700

    final touches for the night idek what i changed

--- nav/nav.js
+++ nav/nav.js
@@ -9,7 +9,7 @@ class SiteNav extends HTMLElement {
           <li><a href="index.html" class="nav-link">Work</a></li>
           <li><a href="play.html" class="nav-link">Play</a></li>
           <li><a href="about.html" class="nav-link">About</a></li>
-          <li><a href="resume.html" class="nav-link">Resume</a></li>
+          <li class="highlight" style="padding-top: 2px;">Resume</li>
         </ul>
       </nav>
     `;
