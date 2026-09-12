6c3ebd4a34530a70bca127a5b0ce37d2ba2234a0
Olivia Blackmore <olivia_blackmore@sfu.ca>
2026-09-12 13:58:46 -0700

    footer changed

--- footer/footer.js
+++ footer/footer.js
@@ -3,13 +3,12 @@ class SiteFooter extends HTMLElement {
     this.innerHTML = `
       <footer class="footer">
         <div class="footer-left">
-          <p>Coded w/ HTML, CSS, and a little JavaScript :)</p>
-          <p class="footer-muted">Last Updated AUG 2026</p>
+          <p>Olivia Blackmore :) <br> <span class="highlight">Built with time, learning, and some fun.</span> </p>
         </div>
         <div class="footer-right">
-          <a href="https://www.linkedin.com/in/olivia--blackmore" class="footer-link" target="_blank">LinkedIn ></a>
-          <a href="mailto:omb5@sfu.ca" class="footer-link">Email ></a>
-          <a href="Resume2026OB.pdf" class="footer-link" target="_blank">Resume ></a>
+          <a href="https://www.linkedin.com/in/olivia--blackmore" class="footer-link" target="_blank">LinkedIn</a>
+          <a href="Resume2026OB.pdf" class="footer-link" target="_blank">Resume</a>
+          <a href="mailto:omb5@sfu.ca" class="footer-link">omblackmore@gmail.com</a>
         </div>
       </footer>
     `;
--- nav/nav.js
+++ nav/nav.js
@@ -14,7 +14,7 @@ class SiteNav extends HTMLElement {
           <li><a href="index.html#work" class="nav-link">Work</a></li>
           <li><a href="play.html" class="nav-link">Play</a></li>
           <li><a href="about.html" class="nav-link">About</a></li>
-          <li><a href="Resume2026OB.pdf" class="nav-link">Resume</a></li>
+          <li><a href="Resume2026OB.pdf" class="nav-link" style="text-decoration: underline;">Resume</a></li>
         </ul>
       </nav>
     `;
